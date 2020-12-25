41105cfdcf4d975604b4401505c07b1437255ef6
MikeyBerger <michaeljudahberger@gmail.com>
2020-12-25 09:59:51 -0800

    Random Spawning Implemented
    Improvements are needed regarding random spawning

---
+++ APCreateTask(SpaceBlasters)/Assets/ChangeColor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    private PlayerColor PC;
+    private SpriteRenderer SR;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PC = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerColor>();
+        SR = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PC.PlayerNum == 0)
+        {
+            SR.color = Color.red;
+        }
+        else if (PC.PlayerNum == 1)
+        {
+            SR.color = Color.blue;
+        }
+        else if (PC.PlayerNum == 2)
+        {
+            SR.color = Color.yellow;
+        }
+        else if (PC.PlayerNum == 3)
+        {
+            SR.color = Color.green;
+        }
+    }
+}
---
+++ APCreateTask(SpaceBlasters)/Assets/PlayerColor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerColor : MonoBehaviour
+{
+    private PlayerInput PI;
+    public float PlayerNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PI = GetComponent<PlayerInput>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PI.playerIndex == 0)
+        {
+            PlayerNum = 0;
+        }
+        else if (PI.playerIndex == 1)
+        {
+            PlayerNum = 1;
+        }
+        else if (PI.playerIndex == 2)
+        {
+            PlayerNum = 2;
+        }
+        else if (PI.playerIndex == 3)
+        {
+            PlayerNum = 3;
+        }
+    }
+}
