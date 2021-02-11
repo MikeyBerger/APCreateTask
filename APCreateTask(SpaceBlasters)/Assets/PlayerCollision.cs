6a6af8d1ccc212f38fc3dd0fb2973b4b506f5560
MikeyBerger <michaeljudahberger@gmail.com>
2021-02-11 11:21:00 -0800

    Project Saved

---
+++ APCreateTask(SpaceBlasters)/Assets/PlayerCollision.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    private PlayerController PC;
+    private ScoreKeeper SK;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PC = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        SK = GameObject.FindGameObjectWithTag("GameController").GetComponent<ScoreKeeper>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Green" && PC.IsShooting)
+        {
+            SK.Score = SK.Score + SK.ScoreIncrease;
+        }
+
+        if (collision.gameObject.tag == "Red" && PC.IsShooting)
+        {
+            SK.Score = SK.Score - SK.ScoreDecrease;
+        }
+    }
+}
